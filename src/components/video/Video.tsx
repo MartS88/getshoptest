d41db8beb5c5ee9b9cd4d1054335d24fdb14ce2d
Martin <papapopal522@gmail.com>
2023-10-24 12:48:23 +0300

    first

--- src/components/video/Video.tsx
+++ src/components/video/Video.tsx
@@ -1,29 +1,24 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const MyVideoComponent = () => {
-
-
-
-
     const videoId = 'M7FIvfx5J10';
 
-    const opts = {
-        height: '720',
-        width: '1280',
-        playerVars: {
+    const iframeRef = useRef<HTMLIFrameElement | null>(null);
 
-            autoplay: 1,
-        },
-    };
+    useEffect(() => {
+        if (iframeRef.current){
+            iframeRef.current.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+        }
+
+    }, [videoId]);
 
     return (
         <div>
             <iframe
-
                 title="YouTube Video"
                 width="1280"
                 height="720"
-                src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
+                ref={iframeRef}
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
@@ -33,3 +28,44 @@ const MyVideoComponent = () => {
 };
 
 export default MyVideoComponent;
+
+
+
+
+
+//
+// import React from 'react';
+//
+// const MyVideoComponent = () => {
+//
+//
+//
+//
+//     const videoId = 'M7FIvfx5J10';
+//
+//     const opts = {
+//         height: '720',
+//         width: '1280',
+//         playerVars: {
+//
+//             autoplay: 1,
+//         },
+//     };
+//
+//     return (
+//         <div>
+//             <iframe
+//
+//                 title="YouTube Video"
+//                 width="1280"
+//                 height="720"
+//                 src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
+//                 frameBorder="0"
+//                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+//                 allowFullScreen
+//             />
+//         </div>
+//     );
+// };
+//
+// export default MyVideoComponent;
